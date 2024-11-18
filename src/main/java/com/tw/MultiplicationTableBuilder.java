b350fe8e953e1a746b4621ae3daddc99dab18ec1
Sylas Qiu <qiusy@oocl.com>
2024-11-18 16:23:54 +0800

    [MultiplicationTable] chrom: rename generateMultiplicationTable function name

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -6,12 +6,12 @@ public class MultiplicationTableBuilder {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int start = 2;
         int end = 4;
-        String multiplicationTable = builder.build(start, end);
+        String multiplicationTable = builder.generateMultiplicationTable(start, end);
 
         System.out.println(multiplicationTable);
     }
 
-    public String build(int start, int end) {
+    public String generateMultiplicationTable(int start, int end) {
         return "";
     }
 }
